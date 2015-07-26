faef5f5b048de5b49e98932c24b71cf7b39140e0
gopikori <gopi.kori@gmail.com>
2015-07-26 21:32:14 +0530

    Started adding sensors summary. Some refactoring.

--- app/src/main/java/com/gopicreations/sensors/MainActivity.java
+++ app/src/main/java/com/gopicreations/sensors/MainActivity.java
@@ -1,41 +1,78 @@
 package com.gopicreations.sensors;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends AppCompatActivity {
 
+    TextView sensorSummaryTextView;
+    ListView listView;
     private List<Sensor> sensors = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Resources res = getResources();
+
         setContentView(com.gopicreations.sensors.R.layout.activity_main);
 
         SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
         sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
 
-        //ArrayAdapter<Sensor> sensorAdapter = new ArrayAdapter(this,
-        //        android.R.layout.simple_list_item_1, sensors.toArray());
-        //setListAdapter(sensorAdapter);
-        setListAdapter(new SensorAdapter(this, 0, sensors));
-        //getListView().setTextFilterEnabled(true);
+        sensorSummaryTextView = (TextView) findViewById(R.id.sensorSummary);
+        sensorSummaryTextView.setText(Html.fromHtml(String.format(res.getString(R.string.sensorlist_summary_count), sensors.size())));
+
+        listView = (ListView) findViewById(R.id.sensorList);
+        listView.setAdapter(new SensorAdapter(this, 0, sensors));
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getBaseContext(), SensorInfoActivity.class);
+                intent.putExtra("sensorInfo", sensors != null ? sensors.get(position).getType() : -1);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        Intent intent = new Intent(this, SensorInfoActivity.class);
-        intent.putExtra("sensorInfo", sensors != null ? sensors.get(position).getType() : -1);
-        startActivity(intent);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(com.gopicreations.sensors.R.menu.menu_sensor_info, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == com.gopicreations.sensors.R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/gopicreations/sensors/SensorInfoActivity.java
+++ app/src/main/java/com/gopicreations/sensors/SensorInfoActivity.java
@@ -1,20 +1,26 @@
 package com.gopicreations.sensors;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 
-public class SensorInfoActivity extends ActionBarActivity {
+public class SensorInfoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Resources res = getResources();
+
         setContentView(com.gopicreations.sensors.R.layout.activity_sensor_info);
         TextView sensorName =(TextView)findViewById(com.gopicreations.sensors.R.id.sensorInfo);
 
@@ -23,33 +29,12 @@ public class SensorInfoActivity extends ActionBarActivity {
 
         String sensorDetails = "";
         sensorDetails += "\n" + sensor.getName();
-        sensorDetails += "\n Vendor : " + sensor.getVendor();
-        sensorDetails += "\n Version : " + sensor.getVersion();
-        sensorDetails += "\n Power : " + sensor.getPower() + "mA";
-        sensorDetails += "\n Resolution : " + sensor.getResolution();
+        sensorDetails += res.getString(R.string.sensorinfo_vendor) + sensor.getVendor();
+        sensorDetails += res.getString(R.string.sensorinfo_version) + sensor.getVersion();
+        sensorDetails += res.getString(R.string.sensorinfo_power) + sensor.getPower() + res.getString(R.string.sensorinfo_power_unit);
+        sensorDetails += res.getString(R.string.sensorinfo_resolution) + sensor.getResolution();
         sensorDetails += "\n";
         sensorName.setText(sensorDetails);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(com.gopicreations.sensors.R.menu.menu_sensor_info, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == com.gopicreations.sensors.R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
